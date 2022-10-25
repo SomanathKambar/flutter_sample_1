77eaffd2bd067065fb9849e9db1f36f7fddbbddb
somanath.kambar <somanath.kambar@gmail.com>
2022-10-25 20:23:22 +0530

    Adding image from assets, network and appicons

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text(
-          "Hello World!",
-          selectionColor: Colors.black38,
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          title: Text("I am Rich "),
+          backgroundColor: Colors.blueGrey,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage("images/tonic.png"),
+            // for netwotk image
+          //   image: NetworkImage(
+          //       "https://cdn.pixabay.com/photo/2022/10/16/13/53/early-morning-7525151_1280.jpg"),
+           ),
         ),
       ),
     ),
