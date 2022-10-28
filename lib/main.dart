40ff989f954ccb860dfd3cf67dc4078ef09cc603
somanath.kambar <somanath.kambar@gmail.com>
2022-10-28 11:53:44 +0530

    Adding simple icon & text inside container

--- lib/main.dart
+++ lib/main.dart
@@ -72,7 +72,35 @@ class CardApp extends StatelessWidget {
                   fontFamily: "Source Sans Pro",
                   fontWeight: FontWeight.bold
                 ),
-              )
+              ),
+              Container( color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                children: [
+                  Icon(Icons.phone, color: Colors.teal,),
+                  SizedBox(width: 10.0,),
+                  Text("+91 123 456 7890",
+                  style: TextStyle(
+                    color: Colors.teal.shade900
+                  ),),
+                ],
+              ),
+              ),
+              Container( color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(Icons.email, color: Colors.teal,),
+                    SizedBox(width: 10.0,),
+                    Text("tonic@bottle.com",
+                      style: TextStyle(
+                          color: Colors.teal.shade900
+                      ),),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
