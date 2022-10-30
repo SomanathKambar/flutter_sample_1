9c2a49ade94a59f08b0febd74d9570d63049c22c
somanath.kambar <somanath.kambar@gmail.com>
2022-10-30 12:00:13 +0530

    wrapping images with button to handle clicks

--- lib/AppWithState.dart
+++ lib/AppWithState.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(children: [
-        Expanded(child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Image.asset("images/dice1.png"),
-        ),
-          flex: 1,
-        ),
-        Expanded(child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Image.asset("images/dice1.png"),
-        ),
-        flex: 1,)
-      ],),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: MaterialButton(
+              onPressed: () {
+                print("button 1 pressed");
+              },
+              child: Image.asset("images/dice1.png"),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: MaterialButton(
+                onPressed: () {
+                  print("button 2 pressed");
+                },
+                child: Image.asset("images/dice1.png")),
+          )
+        ],
+      ),
     );
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
   runApp( MaterialApp(
     home: Scaffold(backgroundColor: Colors.red,
       appBar: AppBar(title: Text("Dicee"),
+        centerTitle: true,
         backgroundColor: Colors.red,
       ),
       body: DicePage(),
